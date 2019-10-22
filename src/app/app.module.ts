940f8151cb8d7204e412ea873b41793a4199fee1
b8ibmjava20@iiht.tech <b8ibmjava20@iiht.tech>
2019-10-22 17:42:04 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
+
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+ 
+    LandingPageComponent,
+    
   ],
   imports: [
     BrowserModule,
